06a88dccbb27344b63cbc035f3ed127b7f4834d0
yihanYozikua <newy860318@gmail.com>
2020-11-17 17:21:03 +0800

    add .h & .cpp files

--- main.cpp
+++ main.cpp
@@ -12,45 +12,15 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/imgproc.hpp>
+#include "tools.h"
 
 using namespace std;
 using namespace cv;
 
 int main(  ){
 
-  //============= INITIALIZE VIDEO CAPTURE =================
-  Mat frame;
-  VideoCapture cap(0);
-  
-  int deviceID = 0; // 0: open default camera
-  int apiID = CAP_ANY; // 0: autodetect default API
-
-  // check if succeed
-  if( !cap.isOpened() ){
-    cerr << "ERROR! Unable to open camera\n" ;
-    return -1;
-  }
-
-  //============= GRAB AND WRITE LOOP =================
-  cout << "Start grabbing" << endl
-       << "Press any key to terminate;" << endl;
-
-  while( true ){
-    // check if succeed
-    if( !cap.read( frame ) ){
-      break;
-    }
-    Mat src = Mat( frame );
-
-    // show live and wait for a key with timeout long enough to show images
-    imshow( "Live", src );
-
-    if( waitKey(30) >= 0 ){
-      break;
-    }
-
-    // waitKey( 30 );
-  }
+  Tools test;
+  test.video_capture();
 
   // Mat img = imread( "ttt.png" );
   // if( img.empty() ){
@@ -64,46 +34,3 @@ int main(  ){
 
   return 0;
 }
-
-// Open camera
-void openCamera(){
-  
-  Mat frame;
-
-  //============= INITIALIZE VIDEO CAPTURE =================
-  VideoCapture cap; 
-  
-  int deviceID = 0; // 0: open default camera
-  int apiID = CAP_ANY; // 0: autodetect default API
-
-  // open selected camera using selected API
-  cap.open( deviceID, apiID );
-
-  // check if succeed
-  if( !cap.isOpened() ){
-    cerr << "ERROR! Unable to open camera\n" ;
-    // return -1;
-  }
-
-  //============= GRAB AND WRITE LOOP =================
-  cout << "Start grabbing" << endl
-       << "Press any key to terminate;" << endl;
-
-  while( 1 ){
-    // wait for a new frame frame from camera and store it into 'frame'
-    cap.read( frame );
-    // check if succeed
-    if( frame.empty() ){
-      cerr << "ERROR! blank frame grabbed\n";
-      break;
-    }
-
-    // show live and wait for a key with timeout long enough to show images
-    imshow( "Live", frame );
-    if( waitKey(5) >= 0 ){
-      break;
-    }
-  }
-
-}
-
---
+++ tools.h
@@ -0,0 +1,9 @@
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+class Tools{
+
+  public:
+    void video_capture();
+
+};
