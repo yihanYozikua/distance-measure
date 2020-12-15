5595526cb0f82fb36ae300a568c9279dd48c3cbd
yihanYozikua <newy860318@gmail.com>
2020-12-15 14:12:03 +0800

    20201215 update

---
+++ distance-analyze-GUI/distance-analyze-GUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  distance-analyze-GUI
+//
+//  Created by 蕭伊涵 on 2020/11/24.
+//  Copyright © 2020 YIHAN HSIAO. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
