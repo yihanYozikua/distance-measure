ddefcd2b733a3bb5efa885aeaf81bfe8f0ac59db
yihanYozikua <newy860318@gmail.com>
2021-01-25 18:20:40 +0800

    import firebase lib & init firestore

--- python/py-db.py
+++ python/py-db.py
@@ -1,3 +1,16 @@
-
-
 print("Successfully passed this Python program from C++!")
+
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# import private key
+# path/to/serviceAccount.json 請用自己存放的路徑
+cred = credentials.Certificate('./serviceAccount.json')
+
+# init firebase /* MENTION: one should not init repeatly */
+firebase_admin.initialize_app(cred)
+
+# init firestore
+db = firestore.client()
